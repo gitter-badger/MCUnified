fbe348ea6e826be6290e817a84b9b2e1393639e1
Liz Fransen <lizfransen97@gmail.com>
2016-07-01 22:45:19 -0500

    Added SimpleCurseArtifact.

---
+++ src/main/java/tk/freetobuild/mcunified/curse/SimpleCurseArtifact.java
@@ -0,0 +1,10 @@
+package tk.freetobuild.mcunified.curse;
+
+/**
+ * Created by liz on 7/1/16.
+ */
+public class SimpleCurseArtifact extends CurseArtifact {
+    public SimpleCurseArtifact(String modId, String fileID) {
+        super(modId, "", "", fileID, null);
+    }
+}
